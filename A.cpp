8fd5151b3622d37452b193fbba86d968a0a88fa5
F74032099 <F74032099@mail.ncku.edu.tw>
2015-03-16 20:43:37 +0800

    lab1

---
+++ A.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main()
+
+{
+   int n,m;
+
+   cin >> n;
+
+   cout << n <<" ";
+
+while(n!=1)
+{
+
+
+
+
+   if( (n%2) !=0){
+      n=n*3+1;
+      cout << n <<" ";
+   }
+
+
+   else{
+      n=n/2;
+      cout << n <<" ";
+   }
+
+
+}
+
+cout << endl ;
+return 0;
+
+
+
+
+}
+
